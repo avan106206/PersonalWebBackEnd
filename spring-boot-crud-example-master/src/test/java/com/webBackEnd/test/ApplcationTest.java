d9f14aa827f1720bf84e943907443375fab1d79a
avan106206 <33173525+avan106206@users.noreply.github.com>
2020-05-16 20:14:16 +0800

    16/5/2020 upload to heroku and connect herokuDB

---
+++ spring-boot-crud-example-master/src/test/java/com/webBackEnd/test/ApplcationTest.java
@@ -0,0 +1,13 @@
+package com.webBackEnd.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes=com.webBackEnd.springbootMain.class)
+class ApplcationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
